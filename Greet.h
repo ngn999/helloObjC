09361f0e8206c70adcfffb01c3ff44d2a57757c2
tangyi <ngn998@gmail.com>
2013-04-24 21:37:57 +0800

    add some comment.

--- Greet.h
+++ Greet.h
@@ -2,7 +2,9 @@
 
 // 必须有一个父类?
 @interface Greet : NSObject {
-
+    // 声明实例变量
 }
+
+// 声明方法
 - (void) hello:(NSString*)arg1 :(NSString*)arg2;
 @end
--- Greet.m
+++ Greet.m
@@ -1,7 +1,7 @@
 #import "Greet.h"
 
 @implementation Greet
-
+// 第二个":"的label是可以省掉的.
 - (void) hello:(NSString*)arg1 :(NSString*)arg2 {
     NSLog(@"%@, %@", arg1, arg2);
 }
